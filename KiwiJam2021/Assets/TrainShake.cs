53a9efe1613f94845bbb8f468c936b96a83b80e3
Josh <joshuaeric.malcolm@gmail.com>
2021-07-24 02:30:34 +1200

    Prototype

---
+++ KiwiJam2021/Assets/TrainShake.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrainShake : MonoBehaviour
+{
+    [SerializeField] float speed = 1.0f; //how fast it shakes
+    [SerializeField] float amount = 0.1f; //how much it shakes
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x, (Mathf.Sin(speed * Time.deltaTime) * amount) -2.5f, transform.position.z);
+    }
+}
--- KiwiJam2021/Assets/_Scripts/Player.cs
+++ KiwiJam2021/Assets/_Scripts/Player.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Reflection;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -7,16 +8,38 @@ public class Player : MonoBehaviour
     [SerializeField] private Transform leftPos, leftMid, midPos, rightPos, rightMid;
     [SerializeField] private int pos;
     playerInput input;
+    private bool capsuleOn = true;
+    private bool cubeOn = false;
+    private bool rectLong = false;
+    private bool rectSmall = false;
+
+    [SerializeField] private GameObject capsule, cube;
+
     //0 = left, 1 = mid, 2 = right
     private void Awake()
     {
         input = GetComponent<playerInput>();
     }
-
+    private void Start()
+    {
+        resetBools();
+        capsuleOn = true;
+    }
     void Update()
     {
         PlayerInputs();
         MoveCharacter();
+
+        if (capsuleOn)
+        {
+            resetMesh();
+            capsule.SetActive(true);
+        }
+        if (cubeOn)
+        {
+            resetMesh();
+            cube.SetActive(true);
+        }
     }
 
     private void PlayerInputs()
@@ -30,22 +53,44 @@ public class Player : MonoBehaviour
         {
             pos = 1;
         }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            resetBools();
+            capsuleOn = true;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            resetBools();
+            cubeOn = true;
+        }
     }
     
     private void MoveCharacter()
     {
-        float counter;
         switch (pos)
         {
             case -1:
-                transform.position = new Vector3(leftPos.position.x, 0 ,0);
+                transform.position = new Vector3(leftPos.position.x, leftPos.position.y ,0);
                 break;
             case 0:
-                transform.position = new Vector3(midPos.position.x, 0, 0);
+                transform.position = new Vector3(midPos.position.x, midPos.position.y, 0);
                 break;
             case 1:
-                transform.position = new Vector3(rightPos.position.x, 0, 0);
+                transform.position = new Vector3(rightPos.position.x, rightPos.position.y, 0);
                 break;
         }
+    }
+    private void resetBools()
+    {
+        capsuleOn = false;
+        cubeOn = false;
+        rectLong = false;
+        rectSmall = false;
+    }
+    private void resetMesh()
+    {
+        capsule.SetActive(false);
+        cube.SetActive(false);
     }
 }
