f37c61f8ef4e66777daffb4734dbad22ae530c91
Josh <Joshuaeric.malcolm@gmail.com>
2021-07-23 22:04:26 +1200

    added player and movement

---
+++ KiwiJam2021/Assets/_Scripts/Player.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private Transform leftPos, leftMid, midPos, rightPos, rightMid;
+    [SerializeField] private int pos;
+    playerInput input;
+    //0 = left, 1 = mid, 2 = right
+    private void Awake()
+    {
+        input = GetComponent<playerInput>();
+    }
+
+    void Update()
+    {
+        PlayerInputs();
+        MoveCharacter();
+    }
+
+    private void PlayerInputs()
+    {
+        pos += input.getInput();
+        if(pos < -1)
+        {
+            pos = -1;
+        }
+        if(pos > 1)
+        {
+            pos = 1;
+        }
+    }
+    
+    private void MoveCharacter()
+    {
+        float counter;
+        switch (pos)
+        {
+            case -1:
+                transform.position = new Vector3(leftPos.position.x, 0 ,0);
+                break;
+            case 0:
+                transform.position = new Vector3(midPos.position.x, 0, 0);
+                break;
+            case 1:
+                transform.position = new Vector3(rightPos.position.x, 0, 0);
+                break;
+        }
+    }
+}
---
+++ KiwiJam2021/Assets/_Scripts/playerInput.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerInput : MonoBehaviour
+{
+
+    public int getInput()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            return -1;
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            return 1;
+        }
+        return 0;
+    }
+}
