c1c2886f0de358b2eb6347851dd5aec24a1def22
Josh <joshuaeric.malcolm@gmail.com>
2021-07-25 00:50:49 +1200

    Trees added

--- KiwiJam2021/Assets/_Scripts/TileManager.cs
+++ KiwiJam2021/Assets/_Scripts/TileManager.cs
@@ -11,27 +11,52 @@ public class TileManager : MonoBehaviour
     private float tileLength = 10;
     private int amountTilesOnSceen = 15;
     [SerializeField] Transform spawnPoint;
-    int random;
+    [SerializeField] int random;
 
     void Start()
     {
-        for(int i = 0; i < amountTilesOnSceen; i++)
-        {
-            //random = Random.Range(0, backGroundTilePrefabs.Length - 1);
-            SpawnTile();
-        }
+        startSpawn(0);
+        startSpawn(1);
+        startSpawn(0);
+        startSpawn(1);
+        startSpawn(1);
+        startSpawn(0);
+        startSpawn(0);
+        startSpawn(1);
+        startSpawn(0);
+        startSpawn(1);
+        startSpawn(1);
+        startSpawn(0);
+        //for (int i = 0; i < amountTilesOnSceen; i++)
+        //{
+
+        //    SpawnTile();
+        //}
     }
     public void SpawnTile(int prefabIndex = -1)
     {
+ 
         GameObject go;
-        go = Instantiate(backGroundTilePrefabs[0]) as GameObject;
+        go = Instantiate(backGroundTilePrefabs[random]) as GameObject;
         go.transform.SetParent(transform);
         Vector3 newPos = new Vector3(0, 0, 1) * spawnZ;
         newPos = new Vector3(spawnPoint.position.x, spawnPoint.position.y, newPos.z);
         go.transform.position = newPos;
         spawnZ += tileLength;
-
+        random = Random.Range(0, backGroundTilePrefabs.Length - 1);
         //int random = Random.Range(0, backGroundTilePrefabs.Length - 1);
         //Instantiate(backGroundTilePrefabs[random], spawnPoint.transform.position, Quaternion.identity);
     }
+    private void startSpawn(int number) 
+    {
+        GameObject go;
+        go = Instantiate(backGroundTilePrefabs[number]) as GameObject;
+        go.transform.SetParent(transform);
+        Vector3 newPos = new Vector3(0, 0, 1) * spawnZ;
+        newPos = new Vector3(spawnPoint.position.x, spawnPoint.position.y, newPos.z);
+        go.transform.position = newPos;
+        spawnZ += tileLength;
+        random = Random.Range(0, backGroundTilePrefabs.Length - 1);
+    }
+
 }
